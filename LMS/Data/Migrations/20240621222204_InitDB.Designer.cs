c89e870c2d955d8ed8eb4586c24077b693aff4cd
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2024-06-22 01:29:42 +0300

    #Added database files #Fix AuthorID in book entity #Fix LoginPartial to use the user entity

--- LMS/Data/Migrations/20240621222204_InitDB.Designer.cs
+++ LMS/Data/Migrations/20240621222204_InitDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LMS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LMS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240621222204_InitDB")]
+    partial class InitDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.5");
