653d601e66112ecc10658646d3d1a714444ccd1a
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2024-06-21 23:20:24 +0300

    #Added DB configuration

--- LMS/Data/ApplicationDbContext.cs
+++ LMS/Data/ApplicationDbContext.cs
@@ -9,5 +9,19 @@ namespace LMS.Data
             : base(options)
         {
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string projectRootDirectory = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName;
+            string databaseDirectory = Path.Combine(projectRootDirectory, "Data");
+
+            // Ensure the 'dp' directory exists
+            if (!Directory.Exists(databaseDirectory))
+            {
+                Directory.CreateDirectory(databaseDirectory);
+            }
+
+            string databasePath = Path.Combine(databaseDirectory, "LMSDB.db");
+            optionsBuilder.UseSqlite($"Data Source={databasePath}");
+        }
     }
 }
