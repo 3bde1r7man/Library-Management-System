c8391266d518463e7d0290b02af1f7b98d0fa538
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2024-06-21 23:31:07 +0300

    #Added Book & Category entities

---
+++ LMS/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace LMS.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; } = string.Empty;
+
+    }
+}
